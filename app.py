9801392a10a78f4ede592ba95bdae683c127a51c
KasamSaidi <127087493+KasamSaidi@users.noreply.github.com>
2023-12-20 14:11:52 +0100

    Cleaned app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-# Setze deinen TomTom API-Schlüssel hier
 TOMTOM_API_KEY = '1n7hfspttTjYk53H8xAeOcNM53cseplD'
 
 @app.route('/')
@@ -15,13 +14,10 @@ def calculate_route():
     start_location = request.form.get('start_location')
     end_location = request.form.get('end_location')
 
-    # Führe die Anfrage an die TomTom API durch, um die Route zu berechnen
     url = f'https://api.tomtom.com/routing/1/calculateRoute/{start_location}:{end_location}/json'
     params = {'key': TOMTOM_API_KEY}
     response = requests.get(url, params=params)
     route_data = response.json()
-
-    # Verarbeite die Antwort und zeige sie auf der Webseite an (implementiere die Funktion)
 
     return render_template('result.html', route_data=route_data)
 
